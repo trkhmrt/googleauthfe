70f10e91dd635334096b9b1d218a60b760042feb
Tarık Hamarat <80787831+trkhmrt@users.noreply.github.com>
2026-03-08 05:11:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE = "https://45.74.244.146";
+const API_BASE = "https://auth.algorycode.com";
 const GOOGLE_CLIENT_ID =
     "990624623867-o83douun4e0vke2nur5qteo9pr4mmlf8.apps.googleusercontent.com";
 
